fc21dd46f7fa12395faaeee5cc065376f6dfc2d3
Ronald Maj <36747983+ronaldmaj@users.noreply.github.com>
2024-08-16 12:36:43 +1000

    Update gnssanalysis/gn_download - Fix up input argument to analysis_center
    Co-authored-by: Nathan <95725385+treefern@users.noreply.github.com>

--- gnssanalysis/gn_download.py
+++ gnssanalysis/gn_download.py
@@ -881,7 +881,7 @@ def download_product_from_cddis(
         reference_start,
         file_ext,
         long_filename=long_filename,
-        AC=analysis_center,
+        analysis_center=analysis_center,
         timespan=timespan,
         solution_type=solution_type,
         sampling_rate=sampling_rate,
